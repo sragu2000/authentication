b20d5ae5ede87696bc25d50717aeab7e240f06f9
sragu2000 <s.s.raguraj@gmail.com>
2022-08-10 21:40:20 +0530

    login and signup

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express=require("express");
+const mysql=require("mysql");
+const app=express();
+const cors= require("cors");
+app.use(express.json());
+app.use(cors());
+
+
+const db=mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    password:"",
+    database:"reactdb"
+});
+
+app.post("/register",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    db.query(
+        "INSERT INTO users (username,password) values (?,?)",
+        [username,password],
+        (err,result)=>{
+            if(err){
+                console.log(err);
+            }
+            // by printing console.log(result) we can get response packet
+            if(result.affectedRows>=1){
+                res.send({result:true, message:"Signup Success"});
+            }else{
+                res.send({result:false, message:"Signup Failed"});
+            }
+        }
+    )
+})
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    db.query(
+        "select * from users where username=? and password=?",
+        [username,password],
+        (err,result)=>{
+            if(err){
+                res.send({result:false, message:err});
+            }
+            if(result.length>0){
+                //selected row will be available in result variable
+                // res.send({result:true, message:result});
+                // console.log(result);
+                res.send({result:true, message:result});
+            }else{
+                res.send({result:false, message:"No users found"});
+            }
+        }
+    )
+})
+
+app.listen(3001,()=>{
+    console.log("Running Server");
+})
--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from "axios";
 
 function App() {
+  const [usernameReg,setUsernameReg]=useState("");
+  const [userPassword,setPasswordReg]=useState("");
+  const [signupStatus,setsignupStatus]=useState("");
+  const register=()=>{
+    Axios.post("http://localhost:3001/register",{
+      username:usernameReg, 
+      password:userPassword
+    }).then((response)=>{
+      if(response.data.result){
+        setsignupStatus("Signup Success")
+      }else{
+        setsignupStatus("Signup Failed");
+      }
+      console.log(response.data);
+    })
+  }
+
+  const [loginStatus,setLoginStatus]=useState("");
+
+  const [usernameLog,setUsernameLog]=useState("");
+  const [userPasswordLog,setPasswordLog]=useState("");
+  const login=()=>{
+    Axios.post("http://localhost:3001/login",{
+      username:usernameLog, 
+      password:userPasswordLog
+    }).then((response)=>{
+      if(response.data.result){
+        setLoginStatus("User Name: "+response.data.message[0].username)
+      }else{
+        setLoginStatus("Login failed");
+      }
+      console.log(response.data);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="registration">
+        <h1>Registration</h1>
+        <input type="text" onChange={(e)=>{setUsernameReg(e.target.value)}} placeholder="Username"></input><br></br>
+        <input type="text" onChange={(e)=>{setPasswordReg(e.target.value)}} placeholder="Password"></input><br></br>
+        <button onClick={register}>Register</button>
+      </div>
+      <h1>{signupStatus}</h1> <hr></hr>
+      <div className='login'>
+        <h1>Login</h1>
+        <input type="text" onChange={(e)=>{setUsernameLog(e.target.value)}} placeholder='Username'></input>
+        <br></br>
+        <input type="password" onChange={(e)=>{setPasswordLog(e.target.value)}} placeholder='password'></input>
+        <br></br>
+        <button onClick={login} >Login</button>
+      </div>
+      <h1>{loginStatus}</h1>
     </div>
   );
 }
